6ca8f8436d28f8d5921fe38c0be41a33c722ea22
magfon <magnus.hfondenes@gmail.com>
2024-09-09 12:34:04 +0200

    Fiks

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class O1 {
 
 	public static void main(String[] args) {
-		Scanner input1 = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
 		System.out.print("Gi Intekt: ");
-		float intekkt = input1.nextFloat();
+		float intekkt = input.nextFloat();
 
 		float trinn1 = 0.017f;
 		float trinn2 = 0.04f;
@@ -29,18 +29,20 @@ public class O1 {
 		} else if (intekkt >= 208051 && intekkt <= 292850) {
 			skatt = (intekkt-208050) * trinn1;
 		} else if (intekkt >= 292851 && intekkt <= 670000) {
-			skatt = skatt = maxSkatt1 + ((intekkt-292850)* trinn2);
+			skatt = maxSkatt1 + ((intekkt-292850)* trinn2);
 		} else if (intekkt >= 670001 && intekkt <= 937900) {
-			skatt = skatt = maxSkatt2 + ((intekkt-670000)* trinn3);
+			skatt = maxSkatt2 + ((intekkt-670000)* trinn3);
 		} else if (intekkt >= 937901 && intekkt <= 1350000) {
-			skatt = skatt = maxSkatt3 + ((intekkt-937900)* trinn4);
+			skatt = maxSkatt3 + ((intekkt-937900)* trinn4);
 		} else if (intekkt > 1350001) {
-			skatt = skatt = maxSkatt4 + ((intekkt-1350000)* trinn5);
+			skatt = maxSkatt4 + ((intekkt-1350000)* trinn5);
 		} else {
 			System.out.print("Error");
 		}
 
 		System.out.print("Din trinskatt er på " + skatt + "kr");
+		
+		input.close();
 
 	}
 }
